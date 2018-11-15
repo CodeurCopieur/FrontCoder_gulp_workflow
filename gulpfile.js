b281042d300960e2e27010dd6b87b72a58cc9dee
codwerk <louis_widdy@live.fr>
2018-11-16 00:16:53 +0100

    workflow in progress

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,33 @@
 'use strict';
 
-var gulp = require('gulp'),
-    pug = require('gulp-pug');
+var gulp = require('gulp'), // Requis
+    gp = require('gulp-load-plugins')(); // tous les plugins de package.json
 
+// Variables de chemins
+var source = './src'; // dossier de travail
+var destination = './build'; // dossier à livrer
 
+/**
+ * ---------------------- PUG vers HTML ---------------------------
+ */
 gulp.task('pug', function () {
-    return gulp.src('src/pug/pages/*.pug')
-        .pipe(pug({
+    return gulp.src(source + '/pug/pages/*.pug')
+        .pipe(gp.pug({
             pretty:true
         }))
-        .pipe(gulp.dest('build'));
-})
+        .pipe(gulp.dest(destination));
+});
+
+/**
+ * ---------------------- STYLUS vers CSS ---------------------------
+ * CSSO => Minifier CSS
+ */
+gulp.task('stylus', function(){
+    return gulp.src(source + '/static/stylus/main.styl')
+        .pipe(gp.stylus({}))
+        .pipe(gp.autoprefixer({
+            browsers: ['last 10 versions']
+        }))
+        .pipe(gp.csso())
+        .pipe(gulp.dest(destination + '/static/css/'));
+});
