0585e45e3a55e10fc624bc24795bb9d4f727ef98
codwerk <louis_widdy@live.fr>
2018-11-22 00:32:50 +0100

    maj : install imagemin

---
+++ gulp/tasks/img.js
@@ -0,0 +1,9 @@
+module.exports = function () {
+   $.gulp.task('images', function(){
+       return $.gulp.src($.src + '/static/img/**/*.+(png|jpg|gif|svg)')
+       .pipe($.gp.imagemin({
+           interlaced: true
+       }))
+       .pipe($.gulp.dest($.dest + '/static/images'))
+   });
+};
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -3,5 +3,6 @@ module.exports = function () {
         $.gulp.watch($.src + '/pug/**/*.pug', ['pug']);
         $.gulp.watch($.src + '/static/sass/**/**/*.scss', ['sass'])
         $.gulp.watch($.src + '/static/js/main.js', ['scripts'])
+        $.gulp.watch($.src + '/static/img/*', ['images'])
     });
 }
